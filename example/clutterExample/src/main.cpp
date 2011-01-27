d0b6582affe8449eb428dda94d55339fd3046bbd
Jeff Crouse <jefftimesten@gmail.com>
2011-01-27 17:45:17 -0500

    f

--- example/clutterExample/src/main.cpp
+++ example/clutterExample/src/main.cpp
@@ -7,9 +7,8 @@
 //========================================================================
 int main(int argc, char *argv[]){
 	
-	ofAppClutterWindow* window;
-	window = new ofAppClutterWindow(argc, argv);
-	ofSetupOpenGL(window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
+	ofAppClutterWindow window(argc, argv);
+	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
 	
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
